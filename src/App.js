75d33d14aa0ad6e0968b96ade3c6774dd5aade92
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-13 08:52:30 +0100

    Level 100 complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+
+import ModeSelect from "./ModeSelect";
 
 function App() {
-	return <div>You need to checkout a level branch!</div>;
+	const [mode, setMode] = useState(null);
+
+	function handleSelectedMode(selectedMode) {
+		setMode(selectedMode);
+	}
+
+	return (
+		<div className="m-4">
+			<header className="text-3xl mb-8">
+				<h1>Transport For London Line Information</h1>
+			</header>
+			<div className="w-full px-3 mb-4">
+				<ModeSelect handleSelectedMode={handleSelectedMode} />
+				You selected mode: {mode}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/ModeSelect.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+
+function ModeSelect(props) {
+  const [modes, setModes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.tfl.gov.uk/Line/Meta/Modes")
+      .then(res => res.json())
+      .then(data => {
+        setModes(data);
+      });
+  }, []);
+
+  function handleChange(event) {
+    if (event.target.value) {
+      props.handleSelectedMode(event.target.value);
+    }
+  }
+
+  return (
+    <div className="mb-4">
+      <select
+        onChange={handleChange}
+        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+      >
+        <option>Choose a Mode of Transport...</option>
+        {modes.map(mode => {
+          return (
+            <option key={mode.modeName} value={mode.modeName}>
+              {mode.modeName}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+}
+
+export default ModeSelect;
